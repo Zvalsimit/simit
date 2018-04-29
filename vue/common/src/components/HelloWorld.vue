b03f3c925614ff0f89d15b832df1f016fbf1a8b8
shizhouhao <shizhouhao@bendiyun.cn>
2018-04-29 15:05:13 +0800

    vue-page

---
+++ vue/common/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ index }}</h1>
+    <child :state="index" :num="num" @page-go="readData"></child>
+  </div>
+</template>
+
+<script>
+
+import Page from './page'
+
+export default {
+  name: 'index',
+  data () {
+    return {
+      index: 1,
+      num: 43,
+    }
+  },
+  components : {
+    child : Page
+  },
+  methods : {
+    readData (p) {
+      this.index = p.index
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
